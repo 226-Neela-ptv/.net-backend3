cc088b9bccf901e55949f535872f63c2fcaa6f54
226-Neela-ptv <97968284+226-Neela-ptv@users.noreply.github.com>
2022-01-19 10:13:48 +0530

    commited

---
+++ ConsoleApp12/ConsoleApp12/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    interface IProduct
+    {
+        public string GetProductName();
+        public string GetProductPrice();
+
+    }
+    
+   
+
+}
